a3cd7c3bf1f5d158275c86d4aa5e0a84d7c45e33
Nick Thompson <dev@nickthompson.guru>
2017-09-30 01:33:30 -0400

    Comments for the Comment class

--- app/src/main/java/guru/nickthompson/redditapi/Comment.java
+++ app/src/main/java/guru/nickthompson/redditapi/Comment.java
@@ -3,9 +3,8 @@ package guru.nickthompson.redditapi;
 import java.util.Date;
 
 /**
- * Created by nick on 9/29/17.
+ * Represents a comment in a Reddit {@link Post}.
  */
-
 public class Comment {
     private final Post post;
     private final String commentID;
@@ -13,7 +12,15 @@ public class Comment {
     private final Date timestamp;
     private final String body;
 
-
+    /**
+     * Creates a comment.
+     *
+     * @param post the {@link Post} this comment is in.
+     * @param commentID the ID of the comment.
+     * @param username the username of the user that posted the comment.
+     * @param timestamp the time the comment was originally posted.
+     * @param body the content of the comment.
+     */
     public Comment(Post post, String commentID, String username, Date timestamp, String body) {
         this.post = post;
         this.commentID = commentID;
@@ -22,18 +29,30 @@ public class Comment {
         this.body = body;
     }
 
+    /**
+     * @return the ID of this comment.
+     */
     public String getID() {
         return this.commentID;
     }
 
+    /**
+     * @return the username of the user that posted the comment.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * @return the time that the comment was originally posted.
+     */
     public Date getTimeStamp() {
         return this.timestamp;
     }
 
+    /**
+     * @return the body of this comment.
+     */
     public String getBody() {
         return this.body;
     }
