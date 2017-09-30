a53288be5be29865c569fa107768d9d4bf5fb22e
William Reed <wreed58@gmail.com>
2017-09-30 15:22:27 -0400

    Templates for redirecting clicked subreddit to next fragment

--- app/src/main/java/guru/nickthompson/livethread/SubredditActivity.java
+++ app/src/main/java/guru/nickthompson/livethread/SubredditActivity.java
@@ -1,9 +1,9 @@
 package guru.nickthompson.livethread;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SubredditActivity extends AppCompatActivity implements SelectSubreddit.OnFragmentInteractionListener {
 
@@ -19,7 +19,8 @@ public class SubredditActivity extends AppCompatActivity implements SelectSubred
     }
 
     @Override
-    public void onFragmentInteraction(Uri uri) {
-
+    public void onSubredditClick(String subredditName) {
+        // TODO: start new fragment with this subreddit
+        Toast.makeText(getApplicationContext(), subredditName, Toast.LENGTH_SHORT).show();
     }
 }
