ef92a84c9f50d1f3c42b4c901a90c3deadc2bb82
William Reed <wreed58@gmail.com>
2017-10-23 10:47:39 -0400

    Added refresh functionality to authentication; displaying basic information now

--- app/src/main/java/io/github/livethread/activities/MainActivity.java
+++ app/src/main/java/io/github/livethread/activities/MainActivity.java
@@ -4,13 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import net.dean.jraw.RedditClient;
 import net.dean.jraw.auth.AuthenticationManager;
 import net.dean.jraw.auth.AuthenticationState;
+import net.dean.jraw.auth.NoSuchTokenException;
 import net.dean.jraw.auth.RefreshTokenHandler;
 import net.dean.jraw.auth.TokenStore;
 import net.dean.jraw.http.UserAgent;
@@ -31,6 +31,10 @@ import io.github.livethread.R;
  */
 public class MainActivity extends AppCompatActivity {
     private TextView tvAuthState;
+    private TextView tvName;
+    private TextView tvCommentKarma;
+    private TextView tvLinkKarma;
+    private RedditClient redditClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // init reddit stuff
-        RedditClient redditClient = new RedditClient(UserAgent.of("android",
+        redditClient = new RedditClient(UserAgent.of("android",
                 "io.github.livethread", "v0.0.1", "rillweed"));
         TokenStore store = new AndroidTokenStore(
                 PreferenceManager.getDefaultSharedPreferences(this));
@@ -49,14 +53,22 @@ public class MainActivity extends AppCompatActivity {
 
         // check auth state and display it
         AuthenticationState authState = AuthenticationManager.get().checkAuthState();
-
         tvAuthState = (TextView) findViewById(R.id.tv_main_auth_state);
         tvAuthState.setText(authState.toString());
-    }
+        tvName = (TextView) findViewById(R.id.tv_main_name);
+        tvCommentKarma = (TextView) findViewById(R.id.tv_main_comment_karma);
+        tvLinkKarma = (TextView) findViewById(R.id.tv_main_link_karma);
 
-    public void login(View view) {
-        // TODO: what should the actual request code be?
-        startActivityForResult(new Intent(this, LoginActivity.class), 1);
+        if (authState == AuthenticationState.NEED_REFRESH) {
+            new AsyncCACRunner<>(new AuthenticateRefresh(((LiveThreadApplication) getApplication())
+                    .getCredentials())).execute();
+        } else if (authState == AuthenticationState.NONE) {
+            // TODO: what should the actual request code be?
+            // TODO: should login be a fragment?
+            startActivityForResult(new Intent(this, LoginActivity.class), 1);
+        } else if (authState == AuthenticationState.READY) {
+            showUserData();
+        }
     }
 
     public static final int REQUEST_CODE = 1;
@@ -66,14 +78,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         System.out.println("rq " + requestCode + " rc " + resultCode);
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
-            Credentials credentials = ((LiveThreadApplication) getApplication()).getCredentials();
             String url = data.getStringExtra("RESULT_URL");
 
-            // TODO make request on seperate thread to authenticate
             AsyncCACRunner<String> authAsync = new AsyncCACRunner<>(new Authenticate(url));
             authAsync.execute();
 
         }
+    }
+
+
+    /**
+     * Show some temporary user data so we know its working.
+     */
+    private void showUserData() {
+        new AsyncCACRunner<>(new BasicInfo()).execute();
     }
 
     /**
@@ -97,7 +115,8 @@ public class MainActivity extends AppCompatActivity {
             OAuthHelper oAuthHelper = AuthenticationManager.get().getRedditClient().getOAuthHelper();
             OAuthData oAuthData = null;
             try {
-                oAuthData = oAuthHelper.onUserChallenge(url, ((LiveThreadApplication) getApplication()).getCredentials());
+                oAuthData = oAuthHelper.onUserChallenge(url,
+                        ((LiveThreadApplication) getApplication()).getCredentials());
             } catch (OAuthException e) {
                 e.printStackTrace();
             }
@@ -109,12 +128,80 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void callback(String username) {
-            Toast.makeText(getApplicationContext(), "Logged in as " + username, Toast.LENGTH_SHORT).show();
+            System.out.println("initial authentication");
+            Toast.makeText(getApplicationContext(), "Logged in as " + username, Toast.LENGTH_SHORT)
+                    .show();
             AuthenticationState authState = AuthenticationManager.get().checkAuthState();
             tvAuthState.setText(authState.toString());
+            showUserData();
         }
     }
 
-    // TODO: need a refresh function
-    // see https://github.com/jorgegil96/JRAW-Android-Sample
+    /**
+     * CAC for handling authentication refresh
+     */
+    class AuthenticateRefresh implements AsyncCommandAndCallback<String> {
+
+        private final Credentials credentials;
+
+        /**
+         * New Authenticate Refresh CAC
+         *
+         * @param credentials the credentials used to authenticate the user
+         */
+        AuthenticateRefresh(Credentials credentials) {
+            this.credentials = credentials;
+        }
+
+        @Override
+        public String command() {
+            try {
+                AuthenticationManager.get().refreshAccessToken(((LiveThreadApplication) getApplication())
+                        .getCredentials());
+            } catch (NoSuchTokenException e) {
+                e.printStackTrace();
+            } catch (OAuthException e) {
+                e.printStackTrace();
+            }
+
+            return AuthenticationManager.get().getRedditClient().getAuthenticatedUser();
+        }
+
+        @Override
+        public void callback(String username) {
+            System.out.println("refresh authenitcation");
+            Toast.makeText(getApplicationContext(), "Logged in as " + username, Toast.LENGTH_SHORT).show();
+            AuthenticationState authState = AuthenticationManager.get().checkAuthState();
+            tvAuthState.setText(authState.toString());
+            showUserData();
+        }
+    }
+
+    /**
+     * CAC for getting basic user Data.
+     * Must be replaced by a more versatile network Queue. too much to create this for every network call.
+     */
+    class BasicInfo implements AsyncCommandAndCallback<Void> {
+
+        int linkKarma;
+        int commentKarma;
+        String name;
+
+        @Override
+        public Void command() {
+            linkKarma = redditClient.me().getLinkKarma();
+            commentKarma = redditClient.me().getCommentKarma();
+            name = redditClient.me().getFullName();
+
+            return null;
+        }
+
+        @Override
+        public void callback(Void result) {
+            System.out.println("set basic info");
+            tvName.setText("Name: " + name);
+            tvCommentKarma.setText("Comment Karma: " + commentKarma);
+            tvLinkKarma.setText("Link Karma: " + linkKarma);
+        }
+    }
 }
